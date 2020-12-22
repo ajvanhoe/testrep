a4cace9fda2f1b5ca5f952fbdb1c310fc360c606
ajvan <iwan.nikolic@mail.ru>
2020-12-22 23:03:51 +0100

    refresh

--- resources/views/theme/partials/javascript.blade.php
+++
@@ -1,4 +0,0 @@
-<script src="{{ asset('vendor/canvas/js/jquery.js') }}"></script>
-<script src="{{ asset('vendor/canvas/js/plugins.js') }}"></script>
-<!-- Footer Scripts	-->
-<script src="{{ asset('vendor/canvas/js/functions.min.js') }}"></script>
