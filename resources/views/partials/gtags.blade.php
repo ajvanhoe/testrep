16f48672e4c7481fd7cdae112a145389f80ee06a
ajvan <iwan.nikolic@mail.ru>
2020-12-22 20:36:39 +0100

    total refresh

--- resources/views/partials/gtags.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127453652-2"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-127453652-2');
-</script> -->
